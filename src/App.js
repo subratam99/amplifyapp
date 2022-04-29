0de548996f8cad2b66102b954a12b8b3be7ad19b
Subrata <subratam99@yahoo.com>
2022-04-29 17:23:43 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +21,15 @@ function App() {
     </div>
   );
 }
-
+*/
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
+}
 export default App;
